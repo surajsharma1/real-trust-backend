2b66fa41b7638932f19bd33cb96b1faa5b81bcaf
surajsharma1 <surajsharma424255@gmail.com>
2026-02-28 13:19:36 +0530

    ch

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true
+}));
 
 // Mount Modular Routes (Paths exactly match Frontend API calls)
 app.use("/api/projects", require("./routes/projectroutes"));
