40e60875dbea0263a84b02d3b89f2aef9cea40f6
surajsharma1 <surajsharma424255@gmail.com>
2026-02-28 13:41:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://real-trust-frontend-exsq.vercel.app'],
     credentials: true
 }));
 
